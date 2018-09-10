765fce123c040e69590abb0053328964cf44cc8b
Connor Rowland <connor.rowland34@gmail.com>
2018-09-10 08:55:31 -0400

    FE Updates to service providers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'core-js/es6/set';
 import 'core-js/es6/map';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
