24bb6ccc7e89186bb71e735ff49548178b52fa7d
Connor Rowland <connor.rowland34@gmail.com>
2018-09-21 12:54:00 -0400

    Updates email

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
       <section id="footer">
         <div className="flex--grid" id="footer--inner">
-          <a href="mailto:helloworld@storyventures.com" id="footer--email">helloworld@storyventures.com</a>
+          <a href="mailto:helloworld@storyventures.vc" id="footer--email">helloworld@storyventures.vc</a>
           <h6 id="footer--copyright">Copyright © 2017 story ventures All Rights Reserved</h6>
         </div>
       </section>
